6dea5c1b4a639d1bc242804a3309270b23bf664c
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-27 23:28:20 -0300

    feat: implementa classe SearchFactory para tratar a opção do menu que o usuário escolher

--- src/strategy/SearchFactory.java
+++ src/strategy/SearchFactory.java
@@ -1,4 +1,13 @@
 package strategy;
 
 public class SearchFactory {
+    public static SearchStrategy getSearch (int option) {
+        if (option == 1){
+            return new TitleSearchStrategy();
+        } else if (option == 2) {
+            return new CategorySearchStrategy();
+        } else{
+            throw new IllegalArgumentException("Opção não esperada: " + option);
+        }
+    }
 }
