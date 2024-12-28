019df472d09c194c540ec5b9d503cdb6a76cd42d
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-28 17:34:25 -0300

    style: acrescenta espaços para melhor visualização do usuário via terminal

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,14 +10,13 @@ public class Main {
         VideoManager videoManager = new VideoManager();
 
         while (true) {
-            System.out.println("\n=== Sistema de Gerenciamento de Vídeos ===");
+            System.out.println("=== Sistema de Gerenciamento de Vídeos ===\n");
             System.out.println("1. Adicionar vídeo");
             System.out.println("2. Listar vídeos");
             System.out.println("3. Pesquisar vídeo por título");
             System.out.println("4. Sair");
             System.out.print("Escolha uma opção: ");
             int opcao = scanner.nextInt();
-
 
             if (opcao == 1) {
                 videoManager.adicionarVideo(scanner);
